6a6c4bbb9f57de0c685120e04d522f1e275fd089
Florian Bury <bury@hectronic.com>
2024-03-01 08:39:07 +0100

    Creating Links

--- obstwiese-web/components/Meadows.tsx
+++ obstwiese-web/components/Meadows.tsx
@@ -21,9 +21,10 @@ export const Meadows = () => {
     return (<div className="grid grid-cols-5 gap-4 ">
         {meadows.map((meadow, meadowIndex) => (
             <div className="shadow  border-gray-300 rounded-xl border bg-owc-warm-orange p-8 m-5" key={meadow.id}>
-                <h1 className="block font-sans text-3xl antialiased font-semibold leading-tight tracking-normal text-inherit">
+                <a href={`/meadows/${meadow.id}`}
+                    className="block font-sans text-3xl antialiased font-semibold leading-tight tracking-normal text-inherit">
                     {meadow.name}
-                </h1>
+                </a>
                 <p className="block py-4 text-2xl font-normal text-gray-500">
                     <p>{meadow.trees.length} trees</p>
                 </p>
