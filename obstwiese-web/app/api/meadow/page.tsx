a9ff2768be72d488522ce03576bf414f03880ca4
Florian Bury <bury@hectronic.com>
2024-03-01 08:40:05 +0100

    Cleanup tests

--- obstwiese-web/app/api/meadow/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-    const { email } = req.body;
-    console.log(req)
-    // Perform server-side operations with `email`
-    res.status(200).json({ message: 'Signup successful' });
-}
