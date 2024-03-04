267b32cd046c93e25699aa455833343997c0e774
Florian Bury <bury@hectronic.com>
2024-03-04 19:22:56 +0100

    CLEAN-UP

--- obstwiese-web/components/EventAdd.tsx
+++ obstwiese-web/components/EventAdd.tsx
@@ -11,15 +11,9 @@ interface NewEventFormProps {
 }
 
 export const NewEventForm = ({parent_id}: NewEventFormProps) => {
-    let  newEventName  = "";
     const [file, setFile] = useState(null);
 
-    const [addEvents] = useMutation(CREATE_EVENT, {
-        variables: { parentID: newEventName, title: newEventName, description: newEventName, timestamp: newEventName, file: file  },
-    })
-
-    const [doUpload, { loading, error }] = useMutation(MULTI_UPLOAD_FILES)
-
+    const [addEvents] = useMutation(CREATE_EVENT)
 
     const handleFileChange = (event) => {
         setFile(event.target.files);
@@ -27,21 +21,7 @@ export const NewEventForm = ({parent_id}: NewEventFormProps) => {
         console.log("File changed2", file);
     };
 
-    const createFileMsgSubmit = async () => {
-        try {
-            console.log("File upload:", file);
-            console.log("File parent_id:", parent_id);
-            await doUpload({
-                variables: {
-                    files: file,
-                    parentID: parent_id
-                },
-            });
-            console.log('File uploaded successfully');
-        } catch (e) {
-            console.error('Error uploading file', e);
-        }
-    };
+    let dateTime = new Date().toISOString().substring(0, 16)
 
 
     const createFileMessage = (formData: FormData) => {
@@ -66,7 +46,7 @@ export const NewEventForm = ({parent_id}: NewEventFormProps) => {
                 <div className=" w-full">
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="datetime-local" id="timestamp" value="2015-01-02T11:42:13.510" name="timestamp" placeholder="Timestamp" required/>
+                        type="datetime-local" id="timestamp" value={dateTime} name="timestamp" placeholder="Timestamp" required/>
                 </div>
                 <div className=" w-full">
                     <textarea
@@ -88,22 +68,6 @@ export const NewEventForm = ({parent_id}: NewEventFormProps) => {
                     </button>
                 </div>
             </form>
-
-            <p className="flex flex-col gap-y-2 items-center p-3">
-                <div className="w-full">
-                    <input onChange={handleFileChange} multiple="multiple"
-                           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                           type="file" id="file" name="file" placeholder="file" required/>
-                </div>
-
-                <input type={`hidden`} value={parent_id} name="treeID"/>
-                <div className="flex justify-between w-full">
-                    <button  onClick={createFileMsgSubmit}
-                            className="bg-owc-vibrant-leaf-green hover:bg-blue-700 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Upload
-                    </button>
-                </div>
-            </p>
         </div>
     );
 }
