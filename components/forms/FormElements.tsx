3c470ea7c9a2ce9c2a2dbe8e2a88bcba3a09c5f1
BurrrY <dev@florian.bury.de>
2024-03-07 21:37:41 +0100

    FIX Missing OnChange of FileInput

--- components/forms/FormElements.tsx
+++ components/forms/FormElements.tsx
@@ -64,6 +64,7 @@ export const OwcFileInput = ({ name, onChange, label }: OwcFileInputProps) => {
                    name={`${name}`}
                    type="file"
                    multiple={true}
+                   onChange={onChange}
                    className="peer h-full w-full border-b border-owc-soft-coral-dark bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-gray-700 outline outline-0 transition-all placeholder-shown:border-owc-soft-coral-dark focus:border-gray-500 focus:outline-0 placeholder:opacity-0 focus:placeholder:opacity-100"/>
             <label
                 className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-owc-soft-coral peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-owc-soft-coral-dark peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-owc-soft-coral">
